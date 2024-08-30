b55a6313bc06610ad9e08098b9b13eb9c8e9549c
19Pawel97 <19pawel9772@gmail.com>
2024-08-30 13:24:07 +0200

    Genre got from master, impl

---
+++ src/main/java/org/example/library/domain/Genre.java
@@ -0,0 +1,40 @@
+package org.example.library.domain;
+
+public enum Genre {
+    FICTION("Fiction"),
+    NON_FICTION("Non-fiction"),
+    MYSTERY("Mystery"),
+    FANTASY("Fantasy"),
+    SCIENCE_FICTION("Science Fiction"),
+    ROMANCE("Romance"),
+    THRILLER("Thriller"),
+    HORROR("Horror"),
+    BIOGRAPHY("Biography"),
+    AUTOBIOGRAPHY("Autobiography"),
+    SELF_HELP("Self-help"),
+    HISTORICAL("Historical"),
+    CLASSICS("Classics"),
+    ADVENTURE("Adventure"),
+    DYSTOPIAN("Dystopian"),
+    YOUNG_ADULT("Young Adult"),
+    CHILDREN("Children"),
+    POETRY("Poetry"),
+    GRAPHIC_NOVEL("Graphic Novel"),
+    TRUE_CRIME("True Crime"),
+    RELIGION("Religion");
+
+    private final String description;
+
+    Genre(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+}
