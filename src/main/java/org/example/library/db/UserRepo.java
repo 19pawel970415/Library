5c73ddcba7f81a6f7349cccedcb3fa203b00a85c
19Pawel97 <19pawel9772@gmail.com>
2024-08-30 16:35:07 +0200

    UserRepo updated

--- src/main/java/org/example/library/db/UserRepo.java
+++ src/main/java/org/example/library/db/UserRepo.java
@@ -1,11 +1,13 @@
 package org.example.library.db;
 
+import lombok.Getter;
 import org.example.library.domain.User;
 import org.example.library.domain.UserGenerator;
 
 import java.io.Serializable;
 import java.util.List;
 
+@Getter
 public class UserRepo implements Serializable {
     private List<User> users = UserGenerator.generateUsers();
 }
