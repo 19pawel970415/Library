2ac0642fb27e221711507a9ad9288884f68d22ac
19Pawel97 <19pawel9772@gmail.com>
2024-08-30 13:18:36 +0200

    Resources and Rental impl

--- src/main/java/org/example/library/domain/BoardGame.java
+++ src/main/java/org/example/library/domain/BoardGame.java
@@ -1,4 +1,12 @@
 package org.example.library.domain;
 
-public class BoardGame extends Resource{
+public class BoardGame extends Resource {
+    private String name;
+    private String numberOfPlayers;
+
+    public BoardGame(String id, String name, String numberOfPlayers) {
+        super(id);
+        this.name = name;
+        this.numberOfPlayers = numberOfPlayers;
+    }
 }
--- src/main/java/org/example/library/domain/Newspaper.java
+++ src/main/java/org/example/library/domain/Newspaper.java
@@ -1,4 +1,12 @@
 package org.example.library.domain;
 
 public class Newspaper extends Resource{
+    private String name;
+    private String edition;
+
+    public Newspaper(String id, String name, String edition) {
+        super(id);
+        this.name = name;
+        this.edition = edition;
+    }
 }
