aa52583d9fd497af93f05d69fee397ea9342ac5b
19Pawel97 <19pawel9772@gmail.com>
2024-08-30 23:21:56 +0200

    Till logged in user menu done

--- src/main/java/org/example/library/service/MenuService.java
+++ src/main/java/org/example/library/service/MenuService.java
@@ -1,9 +1,16 @@
 package org.example.library.service;
 
+import org.example.library.domain.User;
+
+import java.util.InputMismatchException;
+import java.util.Optional;
 import java.util.Scanner;
 
 public class MenuService {
-    private Scanner scanner = new Scanner(System.in);
+
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final UserService USER_SERVICE = new UserService();
+    private static final User USER = new User();
 
     private static void showLoginRequest() {
         System.out.println("Welcome, enter your login:");
@@ -18,19 +25,40 @@ public class MenuService {
         boolean shouldContinue = true;
         do {
             showLoginRequest();
-            String login = scanner.nextLine();
+            String login = SCANNER.nextLine();
             showPasswordRequest();
-            String password = scanner.nextLine();
+            String password = SCANNER.nextLine();
             if (UserValidator.validateUser(login, password)) {
-                System.out.println("Menu:");
-                System.out.println("1. Show my rentals");
-                System.out.println("2. Rent");
-                System.out.println("3. Return");
-                System.out.println("4. Exit");
+                Optional<User> loggedInUser = USER_SERVICE.readUsers().stream()
+                        .filter(u -> u.getLogin().equals(login) && u.getPassword().equals(password))
+                        .findFirst();
+                if (loggedInUser.isPresent()) {
+                    USER.setName(loggedInUser.get().getName());
+                    USER.setSurname(loggedInUser.get().getSurname());
+                    USER.setEmail(loggedInUser.get().getEmail());
+                    USER.setLogin(loggedInUser.get().getLogin());
+                    USER.setPassword(loggedInUser.get().getPassword());
+                    USER.setRentals(loggedInUser.get().getRentals());
+                    System.out.println("Menu:");
+                    System.out.println("1. Show my rentals");
+                    System.out.println("2. Rent");
+                    System.out.println("3. Return");
+                    System.out.println("4. Exit");
+                } else {
+                    System.out.println("Something went wrong, come back later :(");
+                }
                 shouldContinue = false;
             } else {
                 System.out.println("No such user");
             }
         } while (shouldContinue);
+        try {
+            Integer option = SCANNER.nextInt();
+            switch (option) {
+                case 1 -> USER.getRentals().stream().forEach(r -> System.out.println(r.getResource().getId()));
+            }
+        } catch (InputMismatchException ime) {
+            System.err.println("You did not enter a number, you've been logged out. Log in and try again later with numbers");
+        }
     }
 }
