97d102b05c0c6102ba22b4e13c72985e823e17bd
19Pawel97 <19pawel9772@gmail.com>
2024-08-31 15:06:35 +0200

    Generator order of arguments corrected

--- src/main/java/org/example/library/domain/UserGenerator.java
+++ src/main/java/org/example/library/domain/UserGenerator.java
@@ -12,17 +12,17 @@ public class UserGenerator {
     public static final RentalRepo RENTAL_REPO = new RentalRepo();
 
     public static void generateUsers() {
-        Book book1 = new Book("1", "J.K. Rowling", "Harry Potter and the Philosopher's Stone", LocalDateTime.of(1997, 6, 26, 0, 0), Genre.FANTASY);
-        Book book2 = new Book("2", "George Orwell", "1984", LocalDateTime.of(1949, 6, 8, 0, 0), Genre.DYSTOPIAN);
-        Book book3 = new Book("3", "J.R.R. Tolkien", "The Hobbit", LocalDateTime.of(1937, 9, 21, 0, 0), Genre.FANTASY);
+        Book book1 = new Book("Harry Potter and the Philosopher's Stone", "1", "J.K. Rowling", LocalDateTime.of(1997, 6, 26, 0, 0), Genre.FANTASY);
+        Book book2 = new Book("1984", "2", "George Orwell", LocalDateTime.of(1949, 6, 8, 0, 0), Genre.DYSTOPIAN);
+        Book book3 = new Book("The Hobbit", "3", "J.R.R. Tolkien", LocalDateTime.of(1937, 9, 21, 0, 0), Genre.FANTASY);
 
-        BoardGame boardGame1 = new BoardGame("4", "Catan", "3-4");
-        BoardGame boardGame2 = new BoardGame("5", "Ticket to Ride", "2-5");
-        BoardGame boardGame3 = new BoardGame("6", "Carcassonne", "2-5");
+        BoardGame boardGame1 = new BoardGame("Catan", "4", "3-4");
+        BoardGame boardGame2 = new BoardGame("Ticket to Ride", "5", "2-5");
+        BoardGame boardGame3 = new BoardGame("Carcassonne", "6", "2-5");
 
-        Newspaper newspaper1 = new Newspaper("7", "The Times", "Daily Edition");
-        Newspaper newspaper2 = new Newspaper("8", "The Guardian", "Weekend Edition");
-        Newspaper newspaper3 = new Newspaper("9", "The New York Times", "Sunday Edition");
+        Newspaper newspaper1 = new Newspaper("The Times", "7", "Daily Edition");
+        Newspaper newspaper2 = new Newspaper("The Guardian", "8", "Weekend Edition");
+        Newspaper newspaper3 = new Newspaper("The New York Times", "9", "Sunday Edition");
 
         User user1 = new User("John", "Doe", "john.doe@example.com");
 
