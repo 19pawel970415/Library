e5f41da79408a04a1b328b4a799b271c10e0e09a
19Pawel97 <19pawel9772@gmail.com>
2024-08-31 17:36:50 +0200

    Data generating correct in main

--- src/main/java/org/example/library/LibraryApp.java
+++ src/main/java/org/example/library/LibraryApp.java
@@ -1,15 +1,16 @@
 package org.example.library;
 
-import org.example.library.service.MenuService;
-import org.example.library.service.UserService;
+import org.example.library.service.*;
 
 public class LibraryApp {
 
     private static final UserService USER_SERVICE = new UserService();
-    private static final MenuService MENU_SERVICE = new MenuService();
+    private static final ResourceServiceInterface RESOURCE_SERVICE = new ResourceService();
+    private static final MenuServiceInterface MENU_SERVICE = new MenuService();
 
     public static void main(String[] args) {
         USER_SERVICE.writeUsers();
+        RESOURCE_SERVICE.writeResources();
         MENU_SERVICE.showMenu();
     }
 }
