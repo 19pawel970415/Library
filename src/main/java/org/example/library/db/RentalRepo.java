a676dfbfbfe5100fc49f023bf07ae55fcdb4fe5e
19Pawel97 <19pawel9772@gmail.com>
2024-08-31 14:06:16 +0200

    RentalRepo - list and getter added

--- src/main/java/org/example/library/db/RentalRepo.java
+++ src/main/java/org/example/library/db/RentalRepo.java
@@ -1,4 +1,13 @@
 package org.example.library.db;
 
+import lombok.Getter;
+import org.example.library.domain.Rental;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class RentalRepo {
+    private List<Rental> rentals = new ArrayList<>();
+
 }
