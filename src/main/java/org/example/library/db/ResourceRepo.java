dafef1ada779b6283fe73aa8811ba923eda2f1c3
19Pawel97 <19pawel9772@gmail.com>
2024-08-31 16:52:54 +0200

    List of resources created

--- src/main/java/org/example/library/db/ResourceRepo.java
+++ src/main/java/org/example/library/db/ResourceRepo.java
@@ -1,4 +1,12 @@
 package org.example.library.db;
 
+import lombok.Getter;
+import org.example.library.domain.Resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
 public class ResourceRepo {
+    private List<Resource> resources = new ArrayList<>();
 }
