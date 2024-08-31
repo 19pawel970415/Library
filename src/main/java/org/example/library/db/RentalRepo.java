31af92487fa18220c29bd5212a619c76683b6429
19Pawel97 <19pawel9772@gmail.com>
2024-08-31 16:54:01 +0200

    Serializable implemented

--- src/main/java/org/example/library/db/RentalRepo.java
+++ src/main/java/org/example/library/db/RentalRepo.java
@@ -3,11 +3,12 @@ package org.example.library.db;
 import lombok.Getter;
 import org.example.library.domain.Rental;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-public class RentalRepo {
+public class RentalRepo implements Serializable {
     private List<Rental> rentals = new ArrayList<>();
 
 }
--- src/main/java/org/example/library/db/ResourceRepo.java
+++ src/main/java/org/example/library/db/ResourceRepo.java
@@ -3,10 +3,11 @@ package org.example.library.db;
 import lombok.Getter;
 import org.example.library.domain.Resource;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-public class ResourceRepo {
+public class ResourceRepo implements Serializable {
     private List<Resource> resources = new ArrayList<>();
 }
